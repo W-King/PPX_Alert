c052d4282795441d6de5706d8dd68b5ff974156e
jiboxue <jibx@ichsy.com>
2016-10-17 17:10:10 +0800

    Initial Commit

---
+++ PPX_Alert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPX_Alert
+//
+//  Created by pipixia on 16/10/17.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PPX_Alert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PPX_Alert
+//
+//  Created by pipixia on 16/10/17.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
